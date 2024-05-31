737f5f75aa0060ee3ae2a78284b4fe70e33311bb
nebuola <busraberfin24@gmail.com>
2024-05-31 16:35:39 +0300

    added switch for round up option

--- app/src/main/java/com/beecoding/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/beecoding/tipcalculator/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -20,6 +22,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -31,6 +34,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.beecoding.tipcalculator.ui.theme.TipCalculatorTheme
 import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,7 +57,9 @@ fun TipCalculatorApp() {
     var tipPercentageInput by remember { mutableStateOf("15") }
     val tipPercentage = tipPercentageInput.toDoubleOrNull() ?: 15.0
 
-    val tip = calculateTip(amount, tipPercentage)
+    var roundUp by remember { mutableStateOf(false) }
+
+    val tip = calculateTip(amount, tipPercentage, roundUp)
 
     Column(
         modifier = Modifier
@@ -91,19 +97,45 @@ fun TipCalculatorApp() {
                 imeAction = ImeAction.Done
             ),
             modifier = Modifier
-                .padding(bottom = 32.dp)
+                .padding(bottom = 16.dp)
                 .fillMaxWidth()
+        )
+
+        RoundTheTipSwitchRow(
+            isChecked = roundUp,
+            onChecked = { newValue -> roundUp = newValue }
         )
 
         Text(
             text = stringResource(R.string.tip_amount, tip),
             fontWeight = FontWeight.Bold,
             fontSize = 24.sp,
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
             textAlign = TextAlign.Center
         )
 
         Spacer(modifier = Modifier.height(150.dp))
+    }
+}
+
+@Composable
+fun RoundTheTipSwitchRow(
+    isChecked: Boolean,
+    onChecked: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = stringResource(R.string.round_up_tip))
+        Switch(
+            checked = isChecked,
+            onCheckedChange = onChecked
+        )
     }
 }
 
@@ -125,8 +157,16 @@ fun EditNumberField(
     )
 }
 
-private fun calculateTip(billAmount: Double, tipPercent: Double = 15.0): String {
-    val tip = billAmount * tipPercent / 100
+private fun calculateTip(
+    billAmount: Double,
+    tipPercent: Double = 15.0,
+    roundUp: Boolean
+): String {
+    val tip = if (roundUp) {
+        ceil(billAmount * tipPercent / 100)
+    } else {
+        billAmount * tipPercent / 100
+    }
     return NumberFormat.getCurrencyInstance().format(tip)
 }
 
